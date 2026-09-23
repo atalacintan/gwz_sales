d573d6ef0c753a0af17131475b397124672b1db5
atalacintan <atalacin@hotmail.com>
2026-09-23 21:03:13 +0300

    siparişler tarihe göre sıralandı

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,1 +1,1 @@
-SELECT date_date, ROUND(SUM(turnover),2) AS turnover, ROUND(SUM(purchase_cost),2) AS purchase_cost FROM `project-867b1b46-0e32-44ad-84b.course16.gwz_sales` GROUP BY date_date
+SELECT date_date, ROUND(SUM(turnover),2) AS turnover, ROUND(SUM(purchase_cost),2) AS purchase_cost FROM `project-867b1b46-0e32-44ad-84b.course16.gwz_sales` GROUP BY date_date ORDER BY date_date DESC  
